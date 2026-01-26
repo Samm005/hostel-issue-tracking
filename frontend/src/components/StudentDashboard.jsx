54d335270ec4955f6e9d4aa3626cbb2cbd14acf6
Samm005 <samudyathakaranth@gmail.com>
2026-01-26 22:28:33 +0530

    Updated StudentDashboard

--- frontend/src/components/StudentDashboard.jsx
+++ frontend/src/components/StudentDashboard.jsx
@@ -1,30 +1,27 @@
 import { useEffect, useState } from "react";
 import { getIssues } from "../services/api";
 import IssueCard from "./IssueCard";
-import Navbar from "./Navbar";
 
 function StudentDashboard() {
   const [issues, setIssues] = useState([]);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    if (!token) return;
+
     getIssues(token).then(setIssues);
   }, []);
 
   return (
-    <>
-      <Navbar role="student" />
+    <div>
+      <h2>Student Dashboard</h2>
 
-      <div>
-        <h2>Student Dashboard</h2>
+      {issues.length === 0 && <p>No issues reported yet.</p>}
 
-        {issues.length === 0 && <p>No issues reported yet.</p>}
-
-        {issues.map((issue) => (
-          <IssueCard key={issue._id} issue={issue} />
-        ))}
-      </div>
-    </>
+      {issues.map((issue) => (
+        <IssueCard key={issue._id} issue={issue} />
+      ))}
+    </div>
   );
 }
 
