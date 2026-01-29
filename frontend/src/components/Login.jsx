c0160ec5c5019760b71eb8b3c2c146d2af4d876b
Madhuri Sridhar <srdmadhuri1@gmail.com>
2026-01-29 19:00:29 +0530

    Changed login.jsx and login.css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,67 +1,49 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { login } from "../services/api";
 import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const data = await login({ email, password });
-
-      if (!data.token) {
-        alert("Invalid credentials");
-        return;
-      }
-
-      localStorage.setItem("token", data.token);
-      localStorage.setItem("role", data.role);
-
-      if (data.role === "student") {
-        navigate("/student");
-      } else {
-        navigate("/management");
-      }
-    } catch (err) {
-      alert("Login failed");
-    }
-  };
+  const [show, setShow] = useState(false);
 
   return (
-  <div className="dashboard-page login-page">
-    <div className="login-wrapper">
-      <div className="login-container">
-        <h2 className="login-title">Login</h2>
+    <div className="login-page">
+      <div className="login-card">
 
-        <form className="login-form" onSubmit={handleSubmit}>
+        {/* Header */}
+        <div className="logo-section">
+          <div className="logo-circle">SHM</div>
+          <h2>SMART HOSTEL MANAGEMENT</h2>
+          <p>EFFORTLESS HOSTEL MANAGEMENT</p>
+        </div>
+
+        {/* Form */}
+        <input
+          className="login-input"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="password-wrapper">
           <input
             className="login-input"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Password"
+            type={show ? "text" : "password"}
+            placeholder="Enter your password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <span className="eye" onClick={() => setShow(!show)}>👁</span>
+        </div>
 
-          <button className="login-button" type="submit">
-            Login
-          </button>
-        </form>
+        <button className="login-btn">Login</button>
+
+        <p className="signup-text">
+          Don't have an account <span>Create account</span>
+        </p>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default Login;
